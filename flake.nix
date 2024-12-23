f9b37f087f2cd97958d8f3158c4205242b95f2e1
Martin Popovski <martinkozle@yahoo.com>
2024-12-23 16:24:28 +0100

    Add Nvidia hybrid graphics with offloading

--- flake.nix
+++ flake.nix
@@ -5,9 +5,10 @@
     nixpkgs.url = "nixpkgs/nixos-24.11";
     home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... } @ inputs:
+  outputs = { self, nixpkgs, home-manager, nixos-hardware, ... } @ inputs:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
@@ -17,7 +18,7 @@
         p1g3 = nixpkgs.lib.nixosSystem {
           inherit system;
           specialArgs = { inherit inputs; };
-          modules = [ ./configuration.nix ];
+          modules = [ nixos-hardware.nixosModules.lenovo-thinkpad-p1-gen3 ./configuration.nix ];
         };
       };
       homeConfigurations = {
--- home.nix
+++ home.nix
@@ -29,6 +29,13 @@ let
     EOF
     cliphist list | gawk "$prog"
   '';
+
+  patchDesktop = pkg: appName: from: to: lib.hiPrio (
+    pkgs.runCommand "$patched-desktop-entry-for-${appName}" { } ''
+      ${pkgs.coreutils}/bin/mkdir -p $out/share/applications
+      ${pkgs.gnused}/bin/sed 's#${from}#${to}#g' < ${pkg}/share/applications/${appName}.desktop > $out/share/applications/${appName}.desktop
+    '');
+  GPUOffloadApp = pkg: desktopName: patchDesktop pkg desktopName "^Exec=" "Exec=nvidia-offload ";
 in
 {
   # Home Manager needs a bit of information about you and the paths it should
@@ -80,7 +87,7 @@ in
     pkgs.xfce.thunar-volman
     pkgs.xfce.thunar-archive-plugin
     pkgs.xfce.thunar-media-tags-plugin
-    pkgs.godot_4
+    (GPUOffloadApp pkgs.godot_4 "org.godotengine.Godot4")
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
