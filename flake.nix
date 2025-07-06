ece870f69e73ac2ce813d1016a488c28f7722d21
Martin Popovski <martinkozle@yahoo.com>
2025-07-06 19:43:33 +0200

    Update nixos target version to 25.05 and update flake.lock

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "My NixOS flake";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-24.11";
+    nixpkgs.url = "nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
