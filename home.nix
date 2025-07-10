f62d46521d64f85dca4be73b0d5a0eca9031044a
Martin Popovski <martinkozle@yahoo.com>
2025-07-10 20:36:05 +0200

    Migrate migrated zsh.initExtra option to zsh.initContent

--- home.nix
+++ home.nix
@@ -190,7 +190,7 @@ in
       }
     ];
 
-    initExtra = ''
+    initContent = ''
       if uwsm check may-start && uwsm select; then
       	exec systemd-cat -t uwsm_start uwsm start default
       fi
