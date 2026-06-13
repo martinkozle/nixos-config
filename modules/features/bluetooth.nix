84bcf12c2bdce223a8f07005fa462bd5a7d38f1c
Martin Popovski <martinkozle@yahoo.com>
2026-06-13 23:20:46 +0200

    bluetooth: enable powerOnBoot for T14s

--- modules/features/bluetooth.nix
+++ modules/features/bluetooth.nix
@@ -4,7 +4,7 @@
     {
       hardware.bluetooth = {
         enable = true;
-        powerOnBoot = false;
+        powerOnBoot = true;
         settings = {
           General = {
             Experimental = true;
